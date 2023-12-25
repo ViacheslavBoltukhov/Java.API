0c9520873953c1157071691d61bf4174bfae8b6f
ViacheslavBoltukhov <mr.boltukhov@mail.ru>
2023-12-25 11:19:20 +0300

    Add Seminar10

---
+++ scr/seminar10/task1/Box.java
@@ -0,0 +1,41 @@
+package scr.seminar10.task1;
+// Определение обобщенного класса Box с типом-параметром T
+public class Box<T> {
+    // Объявление переменной типа T
+    private T content;
+
+    // Конструктор класса, принимающий объект типа T
+    public Box(T content) {
+        this.content = content;
+    }
+
+    // Метод для получения содержимого коробки
+    public T getContent() {
+        return content;
+    }
+
+    // Метод для установки нового содержимого коробки
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    // Метод для печати информации о содержимом коробки
+    public void printContent() {
+        System.out.println("Содержимое коробки: " + content.toString());
+    }
+}
+
+// Демонстрация использования класса Box
+class Main {
+    public static void main(String[] args) {
+        // Создаем коробку для хранения целых чисел
+        Box<Integer> integerBox = new Box<>(123);
+
+        // Создаем коробку для хранения строк
+        Box<String> stringBox = new Box<>("Привет, мир!");
+
+        // Выводим содержимое коробок
+        integerBox.printContent();
+        stringBox.printContent();
+    }
+}
---
+++ scr/seminar10/task7/NumberProcessor.java
@@ -0,0 +1,52 @@
+package scr.seminar10.task7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Обобщенный класс NumberProcessor с ограничением типа T
+public class NumberProcessor<T extends Number> {
+    private List<T> numbers;
+
+    public NumberProcessor() {
+        this.numbers = new ArrayList<>();
+    }
+
+    // Метод для добавления числа в список
+    public void addNumber(T number) {
+        numbers.add(number);
+    }
+
+    // Метод для обработки чисел с использованием Consumer с ограничением super
+    public void processNumbers(List<? super T> destination) {
+        destination.addAll(numbers);
+    }
+
+    // Метод для вывода чисел
+    public void printNumbers() {
+        for (T num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
+
+// Демонстрация использования NumberProcessor
+class Main {
+    public static void main(String[] args) {
+        // Создание NumberProcessor для Integer
+        NumberProcessor<Integer> integerProcessor = new NumberProcessor<>();
+        integerProcessor.addNumber(10);
+        integerProcessor.addNumber(20);
+
+        // Создание списка для объектов типа Number
+        List<Number> numberList = new ArrayList<>();
+
+        // Обработка чисел и добавление в список Number
+        integerProcessor.processNumbers(numberList);
+
+        // Вывод содержимого numberList
+        System.out.println("Содержимое numberList:");
+        for (Number num : numberList) {
+            System.out.println(num);
+        }
+    }
+}
